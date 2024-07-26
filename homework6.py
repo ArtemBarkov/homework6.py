0b617eae7d010359ee3e66c776d098b7efa51212
ArtemBarkov <artemka.barik@gmail.com>
2024-07-27 00:09:47 +0700

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,21 @@
+my_dict = {'Denis': 2000, 'Artyom': 2002}
+print(my_dict)
+print(my_dict['Denis'])
+print(my_dict.get('Den'))
+my_dict.update({'Max': 2003,
+                'Sahsa': 2004})
+of=my_dict.pop('Max')
+print(of)
+print(my_dict)
+
+
+
+my_set={1,1,2,2,'яблоко','яблоко',42.22,42.22}
+print(my_set)
+my_set.add('string')
+my_set.add('float')
+my_set.discard(2)
+print(my_set)
+
+
+
